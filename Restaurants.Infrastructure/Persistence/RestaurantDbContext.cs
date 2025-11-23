8c41625ed528c799d44fe87dd08699af3386dce6
samcamp12 <jiwang24@gwu.edu>
2025-11-23 18:05:29 -0500

    added migrations for reetsurant

--- Restaurants.Infrastructure/Persistence/RestaurantDbContext.cs
+++ Restaurants.Infrastructure/Persistence/RestaurantDbContext.cs
@@ -6,6 +6,22 @@ namespace Restaurants.Infrastructure.Persistence;
 internal class RestaurantDbContext : DbContext
 {
     internal DbSet<Restaurant> Restaurants { get; set; }
+    internal DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Database=RestaurentsDb;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Application Name=\"SQL Server Management Studio\";Command Timeout=0");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Restaurant>().OwnsOne(r => r.Address);
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(r => r.Dishes)
+            .WithOne()
+            .HasForeignKey(d => d.RestaurantId);
+    }
 }
 
 
