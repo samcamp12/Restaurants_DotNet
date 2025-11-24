e0caa93d821b82ff83ae7039a5a182f6bc5676e8
samcamp12 <jiwang24@gwu.edu>
2025-11-24 01:33:47 -0500

    Add EF Core and infrastructure setup for RestaurantsDb
    - Added `AddInfrastructure` extension method to configure
      `RestaurantDbContext` and register it with DI container.
    - Updated `Program.cs` to use `AddInfrastructure` and retrieve
      the database connection string from configuration.
    - Added `ConnectionStrings` section to `appsettings.Development.json`.
    - Refactored `RestaurantDbContext` to use constructor injection
      for `DbContextOptions` instead of `OnConfiguring`.
    - Updated `Restaurants.API.csproj` to reference
      `Restaurants.Infrastructure` and added EF Core tooling package.

--- Restaurants.Infrastructure/Persistence/RestaurantDbContext.cs
+++ Restaurants.Infrastructure/Persistence/RestaurantDbContext.cs
@@ -3,16 +3,10 @@ using Restaurants.Domain.Entities;
 
 namespace Restaurants.Infrastructure.Persistence;
 
-internal class RestaurantDbContext : DbContext
+internal class RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) : DbContext(options)
 {
     internal DbSet<Restaurant> Restaurants { get; set; }
     internal DbSet<Dish> Dishes { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Database=RestaurentsDb;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Application Name=\"SQL Server Management Studio\";Command Timeout=0");
-    }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
