69a16d85fa5c4eedfcb559d2f290badf56101a9a
samcamp12 <jiwang24@gwu.edu>
2025-12-07 22:43:58 -0500

    Refactor projects and integrate Serilog logging

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,6 +1,7 @@
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
 using Restaurants.Application.Extensions;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,11 @@ builder.Services.AddControllers();
 builder.Configuration.GetConnectionString("RestaurantDb");
 
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Host.UseSerilog((context, config) => config
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", Serilog.Events.LogEventLevel.Information)
+    .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}")
+);
 
 builder.Services.AddApplication();
 
@@ -23,6 +29,8 @@ var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
 await seeder.Seed();
 
 // Configure the HTTP request pipeline.
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
--- Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -13,7 +13,10 @@ public static class ServiceCollectionExtensions
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("RestaurantsDb");
-        services.AddDbContext<RestaurantDbContext>(options => options.UseSqlServer(connectionString));
+        services.AddDbContext<RestaurantDbContext>(options => options
+            .UseSqlServer(connectionString)
+            .EnableSensitiveDataLogging()
+        );
 
         services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
         services.AddScoped<IRestaurantsRepository, RestaurantsRepository>();
